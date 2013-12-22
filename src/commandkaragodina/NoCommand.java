a2d09cf9acfee009ac7482bab38d823df5005653
Светлана <@SVETLANA-PK>
2013-12-23 00:06:41 +0300

    Фиксация реализации пульта

---
+++ src/commandkaragodina/NoCommand.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commandkaragodina;
+
+/**
+ *
+ * @author Светлана
+ */
+public class NoCommand implements Command {
+    @Override
+    public void execute() {}
+}
